cc7f1aaaaf2b46f9b54aea235f4ab771e3b69045
Dante <nguyenchinhan1298@gmail.com>
2022-02-21 17:21:30 +0700

    a

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // Selectors
 let header = document.querySelector('.header');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
-
+window.addEventListener('scroll', function(){
+    let windowPosition = this.window.scrollY > 0;
+    header.classList.toggle('active', windowPosition)
+}) 
 hamburgerMenu.addEventListener('click', function () {
     header.classList.toggle('menu-open')
 })
